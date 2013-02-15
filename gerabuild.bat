303f1bbcc849c8464ecd9e1c2c2c8dfb45e5e637
Alexandre Teles <ateles.1@gmail.com>
2013-02-15 13:58:16 -0200

    MUDANÇA NO GERABUILD.BAT
    Pequenas modificaçõs para tornar o sistema de build mais informativo.

--- gerabuild.bat
+++ gerabuild.bat
@@ -4,6 +4,9 @@
 
 :: Edit by: Alexandre Teles
 :: For use on: Ekaaty Linux
+:: New Usage: "gerabuild 1.0"
+:: To use UPX add "compactar" at the end of the console call
+
 @echo off
 echo Gerador de releases do ekaaty liveusb-creator
 echo O sistema esta gerando a versao %1 do ekaaty liveusb-creator
@@ -25,16 +28,23 @@ cd ..
 echo Gerando arquivo executavel
 python -OO setup.py py2exe
 
-echo Construindo diretorios e copiando arquivos de suporte
+echo Construindo diretorios e copiando arquivos de suporte...
+echo Copiando README.txt
 copy README.txt dist\
+echo Copiando LICENSE.TXT
 copy LICENSE.TXT dist\
+echo Copiando icone liveusb-creator.ico
 copy data\liveusb-creator.ico dist\liveusb-creator.ico
+echo Copiando vcredist_x86
 copy data\vcredist_x86.exe dist\
+echo Copiando script NSIS
 copy data\liveusb-creator.nsi dist\liveusb-creator.nsi
+echo Copiando dll mfc90.dll
 copy tools\mfc90.dll dist\tools\mfc90.dll
-IF EXIST ".\dist\liveusb-creator.exe" (rename ".\dist\liveusb-creator.exe" ekaaty-liveusb-creator.exe) else (echo ERRO CRITICO break)
-IF "%2"=="compactar" (".\build_tools\upx.exe" --ultra-brute ".\dist\ekaaty-liveusb-creator.exe")
-"%programfiles%\NSIS\makensis.exe" dist\liveusb-creator.nsi
+IF EXIST ".\dist\liveusb-creator.exe" (rename ".\dist\liveusb-creator.exe" ekaaty-liveusb-creator.exe) else (echo ERRO CRITICO)
+IF "%2%"=="compactar" (echo Compactando executavel...)
+IF "%2"=="compactar" (".\build_tools\upx.exe" --ultra-brute --compress-icons=3 ".\dist\ekaaty-liveusb-creator.exe")
+IF EXIST dist ("%programfiles%\NSIS\makensis.exe" ".\dist\liveusb-creator.nsi") ELSE (echo Nao foi encontrado um arquivo NSI. Pulando a criacao do instalador)
 IF EXIST dist (rename dist ekaaty-liveusb-creator-%1) ELSE (echo Ocorreu um erro ao criar a pasta de distribuicao)
 IF EXIST build (rmdir /S /Q build)
 
